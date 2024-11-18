50c37722a213664828bbbc6c13fa2523b768c872
Hang Vu <onion209.hust@gmail.com>
2024-11-18 11:01:43 +0700

    Submit bai 2

--- baitap-submit/Hang_Vu/02-llm-api-params/2.improved_bot.py
+++ baitap-submit/Hang_Vu/02-llm-api-params/2.improved_bot.py
@@ -5,19 +5,10 @@ import openai
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 openai.api_key = API_KEY
-def answer_question(question):
+def answer_question(messages):
     try:
         chat_completion = openai.ChatCompletion.create(
-            messages = [
-                {
-                    "role": "system",
-                    "content": "You are a helpful assistant."
-                },
-                {
-                    "role": "user",
-                    "content": question
-                },
-            ],
+            messages = messages,
             max_tokens = 50,
             n = 1,
             temperature = 0.6,
@@ -29,14 +20,26 @@ def answer_question(question):
         return f"Đã xảy ra lỗi: {e}"
 def main():
     print("Welcome to Onion Chatbot. Enter bye when you want to exit")
-    
+    messages = [
+    {
+        "role": "system",
+        "content": "You are a helpful assistant."
+    },]
     while True:
         question = input("Question:")
+        messages.append({
+            "role": "user",
+            "content": question
+        })
         if question.lower() == "bye":
             print("Goodbye. See you soon!")
             break
         else:
-            answer = answer_question(question)
+            answer = answer_question(messages)
+            messages.append({
+                "role": "assistant",
+                "content": answer
+            })
             print(f"Answer: {answer}")
 
 if __name__ == "__main__":
